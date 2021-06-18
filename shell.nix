1dee6cb4f8e2849f37cef4c5a4820696013ff459
James Walker <walkah@walkah.net>
2021-06-17 22:12:48 -0400

    📌 properly pin versions

--- shell.nix
+++ shell.nix
@@ -1,6 +1,11 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell {
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in pkgs.mkShell {
   name = "athens";
   buildInputs = [ pkgs.morph ];
+
+  shellHook = ''
+    export NIX_PATH="nixpkgs=${sources.nixpkgs}:home-manager=${sources.home-manager}:."
+  '';
 }
