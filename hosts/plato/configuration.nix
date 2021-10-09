b2773346ad36e41f5b6eff41f9f64653f5789580
James Walker <walkah@walkah.net>
2021-10-09 18:15:38 -0400

    🔑 plato: enable gnupg agent

--- hosts/plato/configuration.nix
+++ hosts/plato/configuration.nix
@@ -78,6 +78,11 @@ in {
     enable = true;
     promptInit = "";
   };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
