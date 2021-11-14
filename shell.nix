91b3d32222404c76cc473162b2ded8ad9fa75fba
James Walker <walkah@walkah.net>
2021-11-13 21:54:02 -0500

    🔒️ setting up sops-nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,14 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-in pkgs.mkShell {
+in
+pkgs.mkShell {
   name = "athens";
-  buildInputs = [ pkgs.morph ];
+  buildInputs = [
+    pkgs.age
+    pkgs.morph
+    pkgs.sops
+  ];
 
   shellHook = ''
     export NIX_PATH="nixpkgs=${sources.nixpkgs}:home-manager=${sources.home-manager}:."
