96f4f488d4c37d8befe62b063da8d721e9ccc352
James Walker <walkah@walkah.net>
2021-12-19 11:22:21 -0500

    🔧 epicurus: base nix config

--- hosts/epicurus/darwin-configuration.nix
+++ hosts/epicurus/darwin-configuration.nix
@@ -1,9 +1,14 @@
 { config, lib, pkgs, ... }:
+let
+  dotfiles = builtins.fetchTarball
+    "https://github.com/walkah/dotfiles/archive/main.tar.gz";
 
-{
+in {
+  imports = [ <home-manager/nix-darwin> ];
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = [ pkgs.vim ];
+  environment.systemPackages = with pkgs; [ emacs-nox vim ghc stack ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -11,11 +16,30 @@
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
 
-  programs.zsh = {
-    enable = true;
-    promptInit = "";
+  users.users.walkah = {
+    home = "/Users/walkah";
+    shell = pkgs.zsh;
+  };
+
+  home-manager.users.walkah = import "${dotfiles}/home.nix";
+
+  nix = {
+    package = pkgs.nix;
+
+    trustedUsers = [ "root" "@wheel" ];
+
+    extraOptions = ''
+      extra-platforms = x86_64-darwin aarch64-darwin
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  programs = {
+    zsh = {
+      enable = true;
+      promptInit = "";
+    };
   };
 
   # Used for backwards compatibility, please read the changelog before changing.
