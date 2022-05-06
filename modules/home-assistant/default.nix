be14e0698c6acb4f18c0cefdf12a95806f90f372
James Walker <walkah@walkah.net>
2022-05-06 14:39:35 -0400

    ⬆ more version bumps

--- modules/home-assistant/default.nix
+++ modules/home-assistant/default.nix
@@ -5,7 +5,7 @@
   virtualisation.oci-containers = {
     containers = {
       home-assistant = {
-        image = "ghcr.io/home-assistant/home-assistant:2022.5.0";
+        image = "ghcr.io/home-assistant/home-assistant:2022.5.1";
         volumes =
           [ "/var/lib/hass:/config" "/etc/localtime:/etc/localtime:ro" ];
         extraOptions = [ "--privileged" "--network=host" ];
