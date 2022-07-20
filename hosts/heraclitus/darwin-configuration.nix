b7551fa85c67f945438178eb4a979fc01e84b820
James Walker <walkah@walkah.net>
2022-07-19 20:40:10 -0400

    🩹 heraclitus: allowBroken

--- hosts/heraclitus/darwin-configuration.nix
+++ hosts/heraclitus/darwin-configuration.nix
@@ -7,6 +7,8 @@
     ../../modules/builder
   ];
 
+  nixpkgs.config.allowBroken = true;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [ cmake deno elixir emacs go niv nodejs rustup ];
