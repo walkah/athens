846f886c26179b5875dc7d30c931b2d67c2c60e2
James Walker <walkah@walkah.net>
2022-10-20 22:28:21 -0400

    🐛 temporary nix-darwin fix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,9 @@
     deploy-rs.url = "github:serokell/deploy-rs";
 
     darwin = {
-      url = "github:lnl7/nix-darwin/master";
+      # url = "github:lnl7/nix-darwin/master";
+      # TODO remove when merged: https://github.com/LnL7/nix-darwin/pull/552
+      url = "github:Ma27/nix-darwin/fix-manual";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
