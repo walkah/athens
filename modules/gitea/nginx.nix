abf5b4055497419bbab3a58a7724463fc92a8d76
James Walker <walkah@walkah.net>
2023-01-21 17:39:39 -0500

    🔧 gitea proxy config update

--- modules/gitea/nginx.nix
+++ modules/gitea/nginx.nix
@@ -10,6 +10,9 @@ _:
         locations."/" = {
           proxyPass = "http://100.111.208.75:8003";
           proxyWebsockets = true;
+          extraConfig = ''
+            client_max_body_size 0;
+          '';
         };
       };
     };
