2faeb46c599f6c16d6ff381003d9d8bc2d4a3dbd
James Walker <walkah@walkah.net>
2023-01-31 22:01:21 -0500

    ♻️ refactor base config

---
+++ modules/base/common.nix
@@ -0,0 +1,25 @@
+_:
+
+{
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+
+    gc = {
+      automatic = true;
+    };
+
+    settings = {
+      auto-optimise-store = true;
+
+      substituters = [
+        "https://walkah.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "walkah.cachix.org-1:D8cO78JoJC6UPV1ZMgd1V5znpk3jNUERGIeAKN15hxo="
+      ];
+    };
+  };
+}
--- modules/base/darwin.nix
+++ modules/base/darwin.nix
@@ -1,28 +1,19 @@
 _: {
 
+  imports = [ ./common.nix ];
+
   nix = {
     configureBuildUsers = true;
 
     extraOptions = ''
       extra-platforms = x86_64-darwin aarch64-darwin
-      experimental-features = nix-command flakes
     '';
 
     gc = {
       automatic = true;
     };
     settings = {
-      auto-optimise-store = true;
-
-      substituters = [
-        "https://walkah.cachix.org"
-      ];
-
       trusted-users = [ "root" "@admin" ];
-
-      trusted-public-keys = [
-        "walkah.cachix.org-1:D8cO78JoJC6UPV1ZMgd1V5znpk3jNUERGIeAKN15hxo="
-      ];
     };
   };
 
