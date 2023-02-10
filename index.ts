ba09a67ec9bf87526107a6b403c60e21484b2c3a
James Walker <walkah@walkah.net>
2023-02-09 22:37:21 -0500

    🧱 scorates: actual droplet config

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
-// Create a DigitalOcean resource (Domain)
-const domain = new digitalocean.Domain("my-domain", {
-  name: "my-domain.io"
+const socrates = new digitalocean.Droplet("socrates", {
+  backups: true,
+  image: "72067660",
+  ipv6: true,
+  monitoring: true,
+  name: "socrates",
+  privateNetworking: true,
+  region: digitalocean.Region.TOR1,
+  size: digitalocean.DropletSlug.DropletS8VCPU16GB,
+  vpcUuid: "392caea6-dc7f-11e8-b1a9-3cfdfea9ee58",
+}, {
+  protect: true,
 });
 
-// Export the name of the domain
-export const domainName = domain.name;
+export const socratesIP = socrates.ipv4Address;
