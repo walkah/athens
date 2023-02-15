fb0689b904639de38057d890ef343f5cc065a817
James Walker <walkah@walkah.net>
2023-02-15 12:13:26 -0500

    📦 moved packages from dotfiles

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -3,15 +3,31 @@
 {
   environment.systemPackages = with pkgs; [
     emacs
-    devenv
+
+    # Cloud 
+    awscli2
+    pulumi-bin
 
     # Elixir
     elixir
     elixir_ls
 
+    # Git / CI
+    drone-cli
+    mr
+    tea
+
     # Golang
     go
     gopls
+
+    # Nix
+    cachix
+    devenv
+    niv
+    nixfmt
+    nixpkgs-fmt
+    rnix-lsp
 
     # Node/JS
     deno
