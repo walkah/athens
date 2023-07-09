74e75de1682561016f944e199765932c05bcd1d4
James Walker <walkah@walkah.net>
2023-07-09 19:58:27 -0400

    🐛 aristotle: fix bootloader config

--- hosts/aristotle/configuration.nix
+++ hosts/aristotle/configuration.nix
@@ -16,8 +16,7 @@
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = false;
-
+  boot.loader.generic-extlinux-compatible.enable = true;
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
 
   # networking.hostName = "nixos"; # Define your hostname.
