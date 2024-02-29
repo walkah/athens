074e771c3ade599af413c13af25995656b6fecf3
James Walker <walkah@walkah.net>
2024-02-29 16:09:02 -0500

    🔧 give linux-builder more resources

--- modules/builder/default.nix
+++ modules/builder/default.nix
@@ -9,7 +9,22 @@ _: {
         supportedFeatures = [ "benchmark" "big-parallel" "kvm" ];
       }
     ];
-
-    linux-builder.enable = true;
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      speedFactor = 2;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            memorySize = 8 * 1024;
+          };
+          cores = 4;
+        };
+      };
+    };
   };
 }
