8b57a7580a1e09583f8b72321efb6e18e73c1f6a
James Walker <walkah@walkah.net>
2024-09-15 13:06:52 -0400

    🐛 fix chezmoi deploy

--- nix/users/walkah/home.nix
+++ nix/users/walkah/home.nix
@@ -1,19 +1,18 @@
 { lib, pkgs, ... }: {
   home = {
-    username = "walkah";
-    # homeDirectory = if pkgs.stdenv.isDarwin then "/Users/walkah" else "/home/walkah";
-
     packages = with pkgs; [
       chezmoi
       direnv
       eza
       fzf
       git
+      htop
       starship
       tmux
     ];
 
     activation.chezmoi = lib.hm.dag.entryAfter [ "installPackages" ] ''
+      export PATH="${pkgs.git}/bin:$PATH"
       $DRY_RUN_CMD ${pkgs.chezmoi}/bin/chezmoi init --apply walkah/dotfiles
     '';
 
